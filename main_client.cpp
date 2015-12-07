cc69a5be2d56e71e28d899ef12f326587d19b0ab
Shouko <h999342@gmail.com>
2015-12-08 04:21:57 +0800

    connect to server with new Socket class

--- main_client.cpp
+++ main_client.cpp
@@ -8,7 +8,11 @@ int main(int argc, char* argv[]){
 		cerr << "Usage: " << argv[0] << " <hostname> <port>" << endl;
 		return 0;
 	}
-
-
+	Socket s(argv[1], argv[2]);
+	cout << "<< " << s.recv() << endl;
+	s.send("REGISTER#GOOGLE\n");
+	cout << s.recv() << endl;
+	int a;
+	cin >> a;
 	return 0;
 }
