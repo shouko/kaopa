70a0a04e2a5d14f7055e25032eb1258a3ede3708
shouko <h999342@gmail.com>
2015-12-20 07:34:11 +0800

    update server, handle phase 2 required features correctly

--- main_server.cpp
+++ main_server.cpp
@@ -2,90 +2,109 @@
 #include <string.h>
 #include <thread>
 #include <map>
+#include <sstream>
 #include "util.h"
 using namespace std;
 
-SafeQueue<vector<string>*> cmdQueue;
-
 class User{
 public:
-	User(string username) : username(username), port(0), online(0) {
+	string username;
+	int balance;
+	string ip;
+	string port;
+	bool online;
+	User(string username) : username(username), balance(1000), port(""), online(0) {
 	}
 	~User(){
 	}
 	int adjust_balance(int amount){
-		return 0;
+		balance += amount;
+		return balance;
 	}
-	int get_balance(){
-		return 0;
-	}
-	int is_online(){
-		return online;
-	}
-	int set_port(int port){
+	int set_port(string port){
 		this->port = port;
 		this->online = true;
 		return 0;
 	}
-private:
-	string username;
-	int balance;
-	string ip;
-	unsigned short port;
-	bool online;
 };
 
 map<string, User> users;
+map<string, User*> users_online;
+
+int send_list(User* current_user, Socket* c){
+	string uol_str = "";
+	int uol = 0;
+	for(map<string, User*>::iterator it = users_online.begin(); it != users_online.end(); it++){
+		User* user = it->second;
+		uol_str += user->username + "#" + user->ip + "#" + user->port + "\n";
+		uol++;
+	}
+	c->send(to_string(current_user->balance)+ "\n" + to_string(uol) + "\n" + uol_str);
+}
 
 int connection_process(Socket* c){
-	c->send("Hello!");
-	string cmd;
-	User* current_user;
-	while(1){
-		cmd = c->recv();
-		vector<string>* cmd_tokens = split(cmd, "#");
-		if(cmd_tokens->size() != 2){
-			c->send("220 UNKNOWN COMMAND\n");
-		}else if((*cmd_tokens)[0] == "REGISTER"){
-			// register
-			if(users.find((*cmd_tokens)[1]) != users.end()){
-				c->send("220 DUPLICATE USERNAME\n");
-			}else{
-				current_user = new User((*cmd_tokens)[1]);
-				users.insert(make_pair((*cmd_tokens)[1], *(current_user)));
-				c->send("100 SUCCESS\n");
-				break;
-			}
-		}else{
-			// login
-			// fetch user data from user database
-			map<string, User>::iterator it = users.find((*cmd_tokens)[1]);
-			if(it != users.end()){
-				current_user = &(it->second);
-				current_user->set_port(stoi((*cmd_tokens)[1].c_str()));
-				break;
-			}
-		}
-		delete cmd_tokens;
-	}
-/*
-	while(1){
-		cmd = c->recv();
-		vector<string>* cmd_tokens = split(cmd);
-		switch((*cmd_tokens)[0]){
-			case "REGISTER":
-				break;
-			case "List":
-				break;
-			case "Exit":
-				break;
-			default:
-				if(cmd_tokens->size() == 2){
-
+	User* current_user = 0;
+	try{
+		c->send("Hello!");
+		string cmd;
+		while(1){
+			stringstream ss(c->recv());
+			getline(ss, cmd, '#');
+			if(cmd == "REGISTER"){
+				getline(ss, cmd, '\n');
+				if(cmd == ""){
+					// fail
+					continue;
 				}
+				// register
+				if(users.find(cmd) != users.end()){
+					c->send("203 DUPLICATE USERNAME");
+				}else{
+					current_user = new User(cmd);
+					users.insert(make_pair(cmd, *(current_user)));
+					cout << "New user name: " << cmd << endl;
+					cout << users.size() << " th" << endl;
+					c->send("100 SUCCESS");
+					continue;
+				}
+			}else{
+				// login
+				// fetch user data from user database
+				cout << "RECEIVED: " << cmd << endl;
+				map<string, User>::iterator it = users.find(cmd);
+				cout << it->second.username << endl;
+				if(it != users.end()){
+					string port;
+					ss >> port;
+					current_user = &(it->second);
+					current_user->set_port(port);
+					current_user->ip = c->getremoteip();
+					users_online.insert(make_pair(current_user->username, current_user));
+					send_list(current_user, c);
+					break;
+				}else{
+					c->send("200 BAD REQUEST");
+				}
+			}
 		}
+		while(1){
+			stringstream ss(c->recv());
+			getline(ss, cmd);
+			if(cmd == "List"){
+				send_list(current_user, c);
+			}else if(cmd == "Exit"){
+				c->send("Bye");
+				break;
+			}
+		}
+	}catch(SocketException e){
+		cerr << "Error: Connection with client interrupted." << endl;
 	}
-*/
+	map<string, User*>::iterator it = users_online.find(current_user->username);
+	if(it != users_online.end()){
+		users_online.erase(it);
+	}
+	current_user->online = false;
 	delete c;
 	return 0;
 }
@@ -102,6 +121,7 @@ int main(int argc, char* argv[]){
 	int local_port = s->getlocalport();
 	thread (connection_accept, s).detach();
 	cout << "Listening on local port " << local_port << endl;
-
+	char x;
+	cin >> x;
 	return 0;
 }
